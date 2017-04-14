670bfc97e0e0dc136efeb34ad4a78cf4e9279de0
davidpangyear1 <dvpangyr1@hotmail.com>
2017-04-15 00:21:02 +0800

    First stable ./c-sample/tcp

---
+++ c-sample/tcp/readme.txt
@@ -0,0 +1,5 @@
+======================server side======================
+gcc tcp_server.c -o tcp_server.out && ./tcp_server.out
+
+======================client side======================
+gcc tcp_client.c -o tcp_client.out && ./tcp_client.out
--- c-sample/tcp/tcp_server.c
+++ c-sample/tcp/tcp_server.c
@@ -5,8 +5,19 @@
 #include <netinet/in.h>
 #include <arpa/inet.h>
 #include <errno.h>
+#include <stdlib.h>
+
+const int DEFAULT_PORT = 12345;
 
 int getSocketPort(int sock);
+void error(char *msg, int error_no) {
+    printf("Error: %d,%s\n", error_no, msg);
+    exit(1);    /* 1 is passed to parent process */
+}
+
+/*
+    Accept a port argument. If none, use DEFAULT_PORT.
+*/
 
 int main(int argc, char **argv) {
     int sock_p; /* parent socket (file descriptor) */
@@ -16,17 +27,21 @@ int main(int argc, char **argv) {
     struct sockaddr_in addr_client;
     socklen_t len;
     int BACKLOG = 5; /* Refuse connection if <BACKLOG> connection requests are waiting for accept */
+    int temp;
 
     /* check command line arguments */
-    if (argc != 2) {
-        server_port = 12345;
+    if (argc < 2) {
+        server_port = DEFAULT_PORT;
         printf("usage: %s <port>\nNow use default port=%d\n", argv[0], server_port);
+    } else {
+        server_port = atoi(argv[1]);
     }
 
-    /* Create socket, AF_INET means IPv4 */
-    sock_p = socket(AF_INET, SOCK_STREAM, 0);
+    printf("Create socket...\n");
+    sock_p = socket(AF_INET, SOCK_STREAM, 0); /* AF_INET means IPv4 */
+    if (sock_p < 0) error("ERROR opening socket", errno);
 
-    /* Configure socket */
+    printf("Configure socket...\n");
     addr.sin_family = AF_INET;
     addr.sin_port = htons(server_port); /* htons make sure the bytes stored in memory are ordered in network byte order  */
     addr.sin_addr.s_addr = INADDR_ANY;
@@ -34,37 +49,45 @@ int main(int argc, char **argv) {
     /* Set socket option in order to reuse the port. Otherwise, restart this app will cause 'bind' fail. */
     printf("Set socket option...\n");
     int sock_opt = -1;
-    if (setsockopt(sock_p, SOL_SOCKET, SO_REUSEADDR, (void*)&sock_opt, sizeof(sock_opt)) < 0){
-        printf("Error, %d\n", errno);
-        return 0;
-    }
+    temp = setsockopt(sock_p, SOL_SOCKET, SO_REUSEADDR, (void*)&sock_opt, sizeof(sock_opt));
+    if (temp  < 0) error("Error", errno);
 
     printf("bind...\n");
-    if (bind(sock_p, (struct sockaddr*)&addr, sizeof(addr)) < 0) {
-        printf("Error, %d, closing...\n", errno);
+    temp = bind(sock_p, (struct sockaddr*)&addr, sizeof(addr));
+    if (temp  < 0) {
         close(sock_p);
-        return 0;
+        error("Error", errno);
     }
 
     printf("listen...\n");
     if (listen(sock_p, BACKLOG) < 0 ) {
-        printf("Error, %d\n", errno);
-        return 0;
+        close(sock_p);
+        error("Error", errno);
     }
 
     printf("get server port...\n");
     if ((server_port = getSocketPort(sock_p)) < 0) {
-        printf("Error\n");
+        close(sock_p);
+        error("Error", errno);
     }
     printf("server_port=%d\n", server_port);
 
     len = sizeof(addr_client);
     printf("Waiting for connection...\n");
     sock_c = accept(sock_p, (struct sockaddr *)&addr_client, &len);
+    if (sock_c < 0) {
+        close(sock_p);
+        error("Error", errno);
+    }
     printf("Received connection from %s...\n", inet_ntoa(addr_client.sin_addr));
 
-    printf("Send hello...\n");
-    write(sock_c, "HELLO\n", 6);
+    printf("Send 'HELLO\\n'...\n");
+    int n = write(sock_c, "HELLO\n", 6);
+    if (n < 0) {
+        close(sock_p);
+        close(sock_c);
+        error("Error", errno);
+    }
 
     printf("Close client connection...\n");
     close(sock_c);
